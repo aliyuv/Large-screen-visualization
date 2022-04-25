3ed2900f7822aaabf5cda10b3b5c5dca86bfb0ee
aliyu <1324149139@qq.com>
2022-04-25 16:08:30 +0800

    添加chart2

---
+++ src/components/chart-2.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="bordered 管辖统计">
+    <h2>案件破获排名</h2>
+    <div ref=divRef class="chart">
+
+    </div>
+  </div>
+</template>
+
+<script>
+import * as echarts from 'echarts';
+import {baseEchartOptions} from "@/shared/base-echart-options";
+import {createEchartsOptions} from "@/shared/create-echarts-options";
+import {px} from "@/shared/px";
+
+export default {
+  name: "chart-2",
+  mounted() {
+    let myChart = echarts.init(this.$refs.divRef);
+
+    myChart.setOption(createEchartsOptions({
+      ...baseEchartOptions,
+      grid: {
+        x: px(100),
+        y: px(40),
+        x2: px(40),
+        y2: px(40),
+      },
+      xAxis: {
+        type: 'value',
+        boundaryGap: [0, 0.01]
+      },
+      yAxis: {
+        type: 'category',
+        data: ['巴西', '印尼', '美国', '印度', '中国', '世界人口(万)']
+      },
+      series: [
+        {
+          name: '2011年',
+          type: 'bar',
+          data: [18203, 23489, 29034, 104970, 131744, 630230]
+        },
+        {
+          name: '2012年',
+          type: 'bar',
+          data: [19325, 23438, 31000, 121594, 134141, 681807]
+        }
+      ]
+    }));
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,10 +5,13 @@
       <main>
         <section class="section1">
           <chart1/>
+          <chart2/>
         </section>
-
-        <section class="section2"></section>
-        <section class="section3"></section>
+        <section class="bordered section2">
+        </section>
+        <section className="bordered section3"></section>
+        <section className="bordered section4"></section>
+        <section className="bordered section5"></section>
       </main>
     </div>
   </div>
@@ -17,11 +20,12 @@
 <script>
 
 import chart1 from "@/components/chart-1";
+import chart2 from "@/components/chart-2";
 
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: "Home",
-  components: {chart1}
+  components: {chart1, chart2}
 }
 </script>
 
