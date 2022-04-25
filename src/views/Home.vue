e9e1713b4a335e4f2142c99d23dd10fd5b2c4c46
aliyu <1324149139@qq.com>
2022-04-25 19:27:18 +0800

    制作 Chart4

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,8 +9,11 @@
         </section>
         <section class="bordered section2">
           <chart3/>
+          <chart4/>
         </section>
-        <section className="bordered section3"></section>
+        <section className="bordered section3">
+
+        </section>
         <section className="bordered section4"></section>
         <section className="bordered section5"></section>
       </main>
@@ -26,11 +29,12 @@
 import chart1 from "@/components/chart-1";
 import chart2 from "@/components/chart-2";
 import chart3 from "@/components/chart-3";
+import chart4 from "@/components/chart-4";
 
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: "Home",
-  components: {chart1, chart2, chart3}
+  components: {chart1, chart2, chart3, chart4}
 }
 </script>
 
